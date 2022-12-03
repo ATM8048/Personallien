82f8f1478e98255d5aafea21509271187dd21922
Martin Atanasov <martin.atanasov@lernende.bbw.ch>
2022-12-03 11:07:15 +0100

    Projekt hingefügt

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Das sind meine Personallien:");
+        System.out.println("==================================");
+        System.out.println(" ");
+        System.out.println("Mein Name lautet: Atanasov");
+        System.out.println("Mein Vorname lautet: Martin");
+        System.out.println(" ");
+        System.out.print("Ich bin am ");
+        System.out.print("24.01.2000 ");
+        System.out.print("geboren");
+        System.out.println(" ");
+        System.out.print("Ich wohne in: ");
+        System.out.print("8048 ");
+        System.out.print("Zürich");
+    }
+}
